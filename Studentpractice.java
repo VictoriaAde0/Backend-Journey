7f7482fd5c51083b56d58e2da863d950402012fe
VictoriaAde0 <adejumotoria@gmail.com>
2024-04-11 21:26:50 +0100

    added few workings to java file

--- Studentpractice.java
+++ Studentpractice.java
@@ -1,1 +1,17 @@
-g
+public class Studentpractice {
+    public static void main(String[] args) {
+
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        } else {
+            System.out.println("x is less than or equal to 5");
+        }
+    }
+
+    public class Greetings {
+        public static void main(String[] args)
+            System.out.println("Hello, World!");
+        }
+}
+
