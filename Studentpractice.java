26673f14a262c4be4b6b6e5404d0439d79914fea
VictoriaAde0 <adejumotoria@gmail.com>
2024-04-19 14:02:36 +0100

    updated summary

--- Studentpractice.java
+++ Studentpractice.java
@@ -1,3 +1,4 @@
+
 public class Studentpractice {
     public static void main(String[] args) {
 
@@ -12,6 +13,72 @@ public class Studentpractice {
     public class Greetings {
         public static void main(String[] args)
             System.out.println("Hello, World!");
+        }
+
+
+    public class Main {
+        public static void main(String[] args) {
+
+            int a = 5, b = 10, c = 15;
+            System.out.println(a < b && a > c);
+
+            int n = -7;
+            if (n >= 0) {
+                System.out.println("Positive");
+            } else {
+                System.out.println("Negative");
+            }
+            int y;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+            y = sc.nextInt();
+            if (y % 4 == 0) {
+                System.out.println("This number is Even");
+            } else {
+                System.out.println("This number is Odd");
+            }
+        }
+
+        public class pract {
+            public static void Loop(String[] args) {
+//            int i = 1;
+//            while (i < 100) {
+//                System.out.println(i);
+//                i = i * 2;
+//            }
+                for (int i=0;i>=10;i++);
+                {
+                    System.out.println();
+                }
+                int A[]=new int[10];
+                int B[]={1,2,3,4,5,6,7,8,9,10};
+                int C[];
+                C=new int[10];
+                B [2]=15;
+                for(int i=0;i<A.length;i++)
+                {
+                    System.out.println(A[i]);
+                }
+                for(int x:B)
+                {
+                    System.out.println(x++);
+                }
+                for (int x:B)
+                {
+                    System.out.println(x);
+                }
+            }
+            public class Array
+            {
+                public static  void  Array(String[]args)
+            }
+                {
+                    int A[][]=new int[5][5];
+                    int B[][]={{1,2,3},{2,4,6},{1,3,5}};
+                    int C[][];
+                    C=new int[5][5];
+                }
+            }
         }
 }
 
